9fc6e6663aa13b437012efce17ca18f17bb98ae5
nes <nesplayer001@gmail.com>
2026-05-01 22:42:00 +0800

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿async function setLanguage(lang) {
+  const response = await fetch(`lang/${lang}.json`);
+  const data = await response.json();
+
+  document.getElementById('title').textContent = data.title;
+  document.getElementById('description').textContent = data.description;
+}
+
+// 默认加载英文
+setLanguage('en');
